62a5193df82882935ce3ed2cf366dbd0088aef9f
xaviermatroski <141320984+xaviermatroski@users.noreply.github.com>
2024-03-09 01:11:49 +0530

    lex
    created the lexical analyzer

---
+++ src/lexical/lexical_analyser.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <fstream>
+#include "../mini_project/lexical_analyzer.hpp"
+using namespace std;
+
+
+int column = 0;  // character that the lex is currently analyzing
+int word_count = -1;  // token that the lex is on
+int row = 0;  // row that the lex is currrently on
+bool eof{false};
+
+string lex(string file_path){
+    word_count++;
+    if(eof){
+        word_count = 0;
+        eof = false;
+    }
+
+    ifstream MyFile(file_path);
+    string s{""};
+    for(int i = 0; i<=row; i++){
+        getline(MyFile, s);
+    }
+
+    string token{""};
+    while(1){
+        while(s[column] == ' '){
+            column++;
+        }
+        
+        if(s[column] == '\0'){
+            token += "\\n";
+            column = 0;
+            row++;
+            eof = true;
+            return token;
+        }
+        
+        if(s[column] == ','){
+            token += ",";
+            column++;
+            return token;
+        }
+        
+        if(s[column] == '(' || s[column] == ')'){
+            token += s[column];
+            column++;
+            return token;
+        }
+        
+        if(s[column] == ':'){
+            token += s[column];
+            column++;
+            return token;
+        }
+        
+        if(s[column] == '+' || s[column] == '-'){
+            token += s[column];
+            column++;
+        }
+        
+        while(isdigit(s[column])){
+            token += s[column];
+            column++;
+        }
+        
+        if(s[column] == '.'){
+            token += s[column];
+            column++;
+        }
+        
+        if(s[column] == '\"'){
+            token += s[column];
+            column++;
+        }
+        
+        while(isalnum(s[column])){
+            token += s[column];
+            column++;
+        }
+        
+        if(s[column] == '\"'){
+            token += s[column];
+            column++;
+        }
+        
+        return token;
+    }
+}
---
+++ src/lexical/lexical_analyser.hpp
@@ -0,0 +1,5 @@
+#include <string>
+using namespace std;
+
+
+string lex(string file_path);
