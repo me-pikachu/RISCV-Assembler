d8b92e92e7718e57f54ea0be88c47c92cd597bd4
xaviermatroski <141320984+xaviermatroski@users.noreply.github.com>
2024-03-09 23:57:22 +0530

    Update lexical_analyser.cpp
    semicolon as part of the token

--- src/lexical/lexical_analyser.cpp
+++ src/lexical/lexical_analyser.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int column = 0;  // character that the lex is currently analyzing
 int word_count = -1;  // token that the lex is on
 int row = 0;  // row that the lex is currrently on
-bool eof=false;
+bool eof = false;
 
 string lex(string file_path){
     word_count++;
@@ -50,11 +50,11 @@ string lex(string file_path){
             return token;
         }
         
-        if(s[column] == ':'){
-            token += s[column];
-            column++;
-            return token;
-        }
+        // if(s[column] == ':'){
+        //     token += s[column];
+        //     column++;
+        //     return token;
+        // }
         
         if(s[column] == '+' || s[column] == '-'){
             token += s[column];
@@ -76,7 +76,7 @@ string lex(string file_path){
             column++;
         }
         
-        while(isalnum(s[column])){
+        while(isalnum(s[column]) || s[column] == ':'){
             token += s[column];
             column++;
         }
